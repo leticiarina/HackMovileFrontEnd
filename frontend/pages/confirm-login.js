24bfba9f251834fa617249740d04463b4d22bba9
Fabio Romanini <fabioaromanini@gmail.com>
2019-02-17 08:01:01 -0300

    consumes login and generateToken APIS

--- frontend/pages/confirm-login.js
+++ frontend/pages/confirm-login.js
@@ -1,58 +1,51 @@
-import Button from "../components/Button"
-import Card from "../components/Card"
-import Col from "react-bootstrap/Col"
-import { GreenContainer } from "../components/Container"
-import Form from "react-bootstrap/Form"
-import Input from "../components/Input"
-import Row from "react-bootstrap/Row"
+import Button from '../components/Button';
+import Card from '../components/Card';
+import Col from 'react-bootstrap/Col';
+import { GreenContainer } from '../components/Container';
+import Form from 'react-bootstrap/Form';
+import Input from '../components/Input';
+import Row from 'react-bootstrap/Row';
 
-import { connect } from "react-redux"
-import { actions, effects, selectors } from "../store/login"
+import { connect } from 'react-redux';
+import { actions, effects, selectors } from '../store/login';
 
-const ConfirmLogin = ({ 
-  token,
-  onChangeToken,
-  checkToken,
-}) => 
+const ConfirmLogin = ({ token, onChangeToken, checkToken }) => (
   <GreenContainer>
     <Row>
       <Col xs={12}>
         <Card top="193px">
           <h1>Quase lá!</h1>
           <h4>
-            Queremos confirmar que você é você! <br/>
+            Queremos confirmar que você é você! <br />
             Enviamos um código de autenticação para seu número!
           </h4>
           <Form>
             <Form.Group>
               <Input
-                onChange={(e) => onChangeToken(e.target.value)}
+                onChange={e => onChangeToken(e.target.value)}
                 placeholder="Digite aqui o código que recebeu"
-                type="number"
+                type="text"
                 value={token}
-                />
+              />
             </Form.Group>
           </Form>
-          <Button
-            onClick={checkToken}
-            >
-          Enviar 
-          </Button>
+          <Button onClick={checkToken}>Enviar</Button>
         </Card>
       </Col>
     </Row>
   </GreenContainer>
+);
 
-const mapDispatchToProps = (dispatch) => ({
-  onChangeToken: (token) => dispatch(actions.onChangeToken(token)),
-  checkToken: () => dispatch(effects.checkToken()),
-})
+const mapDispatchToProps = dispatch => ({
+  onChangeToken: token => dispatch(actions.onChangeToken(token)),
+  checkToken: () => dispatch(effects.checkToken())
+});
 
-const mapStateToProps = (state) => ({
-  token: selectors.getToken(state),
-})
+const mapStateToProps = state => ({
+  token: selectors.getToken(state)
+});
 
 export default connect(
   mapStateToProps,
-  mapDispatchToProps,
-)(ConfirmLogin)
+  mapDispatchToProps
+)(ConfirmLogin);
--- frontend/store/login/effects.js
+++ frontend/store/login/effects.js
@@ -1,39 +1,39 @@
-import axios from "axios"
-import Router from "next/router"
-import { selectors } from "./"
+import axios from 'axios';
+import Router from 'next/router';
+import { selectors } from './';
 
 export const checkPhone = () => async (dispatch, getState) => {
-  const state = getState()
-  const phoneNumber = selectors.getPhone(state)
+  const state = getState();
+  const phoneNumber = selectors.getPhone(state);
 
   try {
     await axios({
       data: { phoneNumber },
-      method: "post",
-      responseType: "json",
-      url: "http://hackpay-env.wsc8gyb2s4.us-east-1.elasticbeanstalk.com/users/login",
-    })
+      method: 'post',
+      responseType: 'json',
+      url:
+        'http://hackpay-env.wsc8gyb2s4.us-east-1.elasticbeanstalk.com/users/generateToken'
+    });
 
-    Router.push("/confirm-login")
-
-  } catch(error) {
-    console.log(error)
+    Router.push('/confirm-login');
+  } catch (error) {
+    console.log(error);
   }
+};
 
-}
-
-export const checkToken = () => async(dispatch, getState) => {
-  const state = getState()
-  const phoneNumber = selectors.getPhone(state)
-  const token = selectors.getToken(state)
+export const checkToken = () => async (dispatch, getState) => {
+  const state = getState();
+  const phoneNumber = selectors.getPhone(state);
+  const token = selectors.getToken(state);
 
   try {
     const response = await axios({
       data: { phoneNumber, token },
-      method: "post",
-      responseType: "json",
-      url: "http://hackpay-env.wsc8gyb2s4.us-east-1.elasticbeanstalk.com/users/generateToken",
-    })
+      method: 'post',
+      url:
+        'http://hackpay-env.wsc8gyb2s4.us-east-1.elasticbeanstalk.com/users/login',
+      responseType: 'json'
+    });
 
     // const response = {
     //   data: {
@@ -43,12 +43,10 @@ export const checkToken = () => async(dispatch, getState) => {
     //   }
     // }
 
-    if(response.data.status.code == 200) {
-      Router.push("/home")
-    } 
-
-  } catch(error) {
-    console.log(error)
+    if (response.data.status.code == 200) {
+      Router.push('/home');
+    }
+  } catch (error) {
+    console.log(error);
   }
-
-}
+};
